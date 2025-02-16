6303f7f1e812fa4262a2f262abc01e42392538c1
vinicius <vinicius.neves@al.infnet.edu.br>
2025-02-16 18:58:55 -0300

    Adicionando o GameDTO e gameById

--- src/main/java/com/devsuperior/dslist/controllers/GameController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameController.java
@@ -1,13 +1,16 @@
 package com.devsuperior.dslist.controllers;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/games")
@@ -20,12 +23,16 @@ public class GameController {
         this.gameService = gameService;
     }
 
-
     public GameController() {
     }
 
     @GetMapping
     public List<GameMinDTO> findAll() {
         return gameService.findAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    public Optional<GameDTO> findById(@PathVariable Long id) {
+        return gameService.findById(id);
     }
 }
--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dslist.services;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.repositories.GameRepository;
@@ -7,24 +8,41 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 
 @Service
 public class GameService {
 
-  private final GameRepository gameRepository;
+    private final GameRepository gameRepository;
 
-  // Implementar a injeção de dependência através do construtor para facilitar testes
+    // Implementar a injeção de dependência através do construtor para facilitar testes
     @Autowired
-    public GameService(GameRepository gameRepository){
+    public GameService(GameRepository gameRepository) {
         this.gameRepository = gameRepository;
     }
 
+    /**
+     * Método para buscar todos os jogos da lista
+     * @return retorna uma lista com todos os jogos
+     * */
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
         return result.stream().map(GameMinDTO::new).collect(Collectors.toList());
     }
+
+    /**
+     * Método para buscar por Id
+     * @param id id do jogo a ser buscado
+     * @return retorna um game a lista
+     */
+    public Optional<GameDTO> findById(Long id) {
+     return gameRepository.findById(id).map(GameDTO::new);
+    }
+
+
+
 
 
 }
